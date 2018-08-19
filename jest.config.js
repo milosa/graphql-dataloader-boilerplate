0d2e1040c0833c56f23f87d616c17495a64ba76a
Jonathan Cardoso Machado <me@jonathancardoso.com>
2018-08-19 16:06:42 -0300

    feat(tests): improve tests settings to use separated files

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+const ignoredPaths = [
+  '<rootDir>/node_modules/',
+  '<rootDir>/dist',
+  '<rootDir>/scripts',
+  '<rootDir>/repl/',
+  '<rootDir>/flow-typed/',
+  '<rootDir>/test/',
+];
+
+module.exports = {
+  displayName: 'test',
+  testEnvironment: '<rootDir>/test/environment/mongodb',
+  testPathIgnorePatterns: ignoredPaths,
+  coverageReporters: ['lcov', 'html'],
+  coveragePathIgnorePatterns: ignoredPaths,
+  setupTestFrameworkScriptFile: '<rootDir>/test/setupTestFramework.js',
+  globalSetup: '<rootDir>/test/setup.js',
+  globalTeardown: '<rootDir>/test/teardown.js',
+  resetModules: false,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'GraphQL Dataloader Boilerplate Tests',
+        output: './test-results/jest/results.xml',
+      },
+    ],
+  ],
+};
--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,3 @@
 module.exports = () => {
-  console.log('\n# GLOBAL TEST SETUP #');
+  // console.log('\n# GLOBAL TEST SETUP #');
 };
